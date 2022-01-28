5d87f9b6eafa283a28bf1c4b0b99095022ffd9f5
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-28 10:57:40 +0100

    .

---
+++ experimental/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+	double volume = 10.2; //double
+	// CASTING 
+
+	cout << (int) volume << endl;
+	cout << (double) volume << endl;
+
+	return 0;
+
+}
